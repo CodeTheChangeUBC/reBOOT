abc3497089f332fab20d71fdf35e96af8aef09c2
omar2535 <omar2535@gmail.com>
2017-10-30 20:54:53 -0700

    Simple upload button

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', admin.site.urls)
 ]
+
+
--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 from django.shortcuts import render
 
 # Create your views here.
+def get_csv(request):
+    if request.method == 'POST' and request.FILES['my_file']:
+        myfile = request.FILES['my_files']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
 
+
+        return HttpResponse("<h1> It worked </h1>")
+
+    else:
+        return HttpResponse("<h1> It failed </h1> ")
