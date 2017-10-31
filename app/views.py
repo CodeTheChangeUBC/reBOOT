1273f4423177512a94899d8f51b627465d219547
omar2535 <omar2535@gmail.com>
2017-10-30 22:16:01 -0700

    Added events to button.

--- app/views.py
+++ app/views.py
@@ -3,18 +3,20 @@ from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
-
+from .forms import DocumentForm
 from django.shortcuts import render
+from .csvparser import parser
 
 # Create your views here.
 def get_csv(request):
-    if request.method == 'POST' and request.FILES['my_file']:
-        myfile = request.FILES['my_files']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
+    if "POST" == request.method:
 
+        csv_file = request.FILES['my_file']
+        if(csv_file.name.endswith('.csv')):
+            parser(csv_file)
 
-        return HttpResponse("<h1> It worked </h1>")
-
+            return HttpResponse(csv_file)
+        else:
+            return HttpResponse("<h1> Invalid format </h1>")
     else:
-        return HttpResponse("<h1> It failed </h1> ")
+        return HttpResponse("<h1> DId not work</h1> ")
