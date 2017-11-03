3e7c86fe5b3cd2d03f71a40b56ca129dc0daba0d
Omar <omar2535@gmail.com>
2017-11-02 23:47:42 -0700

    Removed uploading in app menu so it wouldn't crash

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ def get_csv(request):
     if "POST" == request.method:
         csv_file = request.FILES.get('my_file', False)
         if(csv_file and csv_file.name.endswith('.csv')):
-            # parser(csv_file)
+            parser(csv_file)
             return render(request, 'app/CSVworked.html')
         else:
             return render(request, 'app/CSVfailed.html')
