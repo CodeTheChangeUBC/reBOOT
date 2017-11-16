65f6f0956086e8fb6d3b831ff7070300c465b2a5
Csignore <ychenvin@gmail.com>
2017-11-16 13:37:45 -0800

    fix issue #31

--- app/admin.py
+++ app/admin.py
@@ -11,18 +11,18 @@ from .models import Item
 # Register your models here.
 #Action for verification
 def make_verified(modeladmin, request, queryset):
-    queryset.update(verified = True)
-    dlist = Donor.objects.all()
-    for d in dlist:
-        d.save()
+	queryset.update(verified = True)
+	dlist = Donor.objects.all()
+	for d in dlist:
+		d.save()
 make_verified.short_description = "Mark as verified"
 
 #Action for unverification
 def make_unverified(modeladmin, request, queryset):
-    queryset.update(verified = False)
-    dlist = Donor.objects.all()
-    for d in dlist:
-        d.save()
+	queryset.update(verified = False)
+	dlist = Donor.objects.all()
+	for d in dlist:
+		d.save()
 make_unverified.short_description = "Mark as unverified"
 
 
@@ -31,43 +31,41 @@ class DonorAdmin(admin.ModelAdmin):
 	fieldsets = [
 		('Donor Contacts',   {'fields': ['donor_name','email', 'telephone_number', 'mobile_number']}),
 		('Details', 	     {'fields': ['want_receipt']}),
-        ('Address',          {'fields': ['address_line','city', 'province', 'postal_code']})
+		('Address',          {'fields': ['address_line','city', 'province', 'postal_code']})
 	]
 	list_display 	= ('id',
-                    'donor_name',
-					'email',
-					'mobile_number',
-					'want_receipt',
-					'verified')
+						'donor_name',
+						'email',
+						'mobile_number',
+						'telephone_number',
+						'want_receipt',
+						'verified')
 	list_filter 	= ['city', 'province']
-	search_fields 	= ['id','donor_name','email', 'telephone_number', 'mobile_number','address_line','city', 'province', 'postal_code']
-
+	search_fields   = ['id', 'donor_name', 'telephone', 'mobile_number', 'address_line', 'city', 'province', 'postal_code', 'email',]
 	def get_donor(self, obj):
 		return obj.id
 	get_donor.short_description = 'Donor ID'
 
 class DonationAdmin(admin.ModelAdmin):
-    fieldsets = [
-		("Donation", 	{'fields': ['donor_id', 'get_donation_donor_name', 'tax_receipt_no', 'donate_date', 'verified']})
-    ]
-    actions = [make_verified, make_unverified]
+	fieldsets = [
+		("Donation", 	{'fields': ['donor_id', 'get_donation_donor_name', 'tax_receipt_no', 'donate_date', 'verified']})]
+	actions = [make_verified, make_unverified]
 
 
-    list_display 	= ('donor_id', 'get_donation_donor_name',
-                       'tax_receipt_no',
-                       'donate_date',
-                       'verified')
-    readonly_fields = ('get_donation_donor_name',)
-    list_filter = []
-    search_fields 	= ['donor_id', 'get_donation_donor_name','tax_receipt_no','donate_date',]
+ 	list_display 	= ('donor_id', 'get_donation_donor_name',
+						'tax_receipt_no',
+						'donate_date',
+						'verified')
+	readonly_fields = ('get_donation_donor_name',)
+	list_filter = []
+	search_fields 	= ['donor_id', 'get_donation_donor_name','tax_receipt_no','donate_date',]
 
 
-    def get_donation_donor_name(self, obj):
-        return obj.donor_id.donor_name
-    get_donation_donor_name.short_description = 'Donor Name'
+	def get_donation_donor_name(self, obj):
+		return obj.donor_id.donor_name
+	get_donation_donor_name.short_description = 'Donor Name'
 
 class ItemAdmin(admin.ModelAdmin):
-
 	fieldsets = [
 		("Item", 	{'fields': ['tax_receipt_no', 'description', 'particulars',
 							'manufacturer','model','quantity', 'working',
@@ -76,8 +74,9 @@ class ItemAdmin(admin.ModelAdmin):
 
 
 	list_display 	= ('get_item', 'tax_receipt_no', 'manufacturer', 'model', 'quantity', 'batch','verified', 'get_donor_name')
-	list_filter 	= ['manufacturer']
-	search_fields 	= ['tax_receipt_no','manufacturer','model', 'batch']
+	list_filter 	= ['manufacturer', 'model', 'working','verified', 'quality']
+	search_fields 	= ['manufacturer','model', 'working', 'batch']
+
 
 
 
