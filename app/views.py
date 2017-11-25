76c185e7b7f2da95d0608e27b3893e9360e922f3
Omar <omar2535@gmail.com>
2017-11-24 22:56:55 -0800

    Added parser from utils to tasks. Added celery.

--- app/views.py
+++ app/views.py
@@ -5,14 +5,15 @@ from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from .forms import DocumentForm
 from django.shortcuts import render
-from .utils import parser
+#from .utils import parser
+from .tasks import parser
 
 # Create your views here.
 def get_csv(request):
     if "POST" == request.method:
         csv_file = request.FILES.get('my_file', False)
         if(csv_file and csv_file.name.endswith('.csv')):
-            parser(csv_file)
+            parser.delay(csv_file)
             return render(request, 'app/CSVworked.html')
         else:
             return render(request, 'app/CSVfailed.html')
--- reboot/settings.py
+++ reboot/settings.py
@@ -36,9 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'djcelery',
+    'kombu.transport.django'
 
 ]
+import djcelery
+djcelery.setup_loader()
+BROKER_URL = "django://"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
