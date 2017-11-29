52a064fb8f9d8e4f64b9d8b09e312f7c16c525e1
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2017-11-29 00:11:24 -0800

    Change aws server and type

--- reboot/settings.py
+++ reboot/settings.py
@@ -82,13 +82,13 @@ WSGI_APPLICATION = 'reboot.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 # Reboot Development Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rebootdbdev',
-        'HOST': 'rebootdbinstance.cn0ttbkdpgt2.ca-central-1.rds.amazonaws.com',
-        'PORT': '3306',
+	'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
         'USER': 'ctc_reboot',
-        'PASSWORD': 'ctcreboot1'
+        'PASSWORD': 'ctcreboot1',
+        'HOST': 'rebootdbdev.cn0ttbkdpgt2.ca-central-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
