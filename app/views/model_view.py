2766521741ceacbc7b0ce37c4647040999b88d56
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-02-23 23:04:24 -0800

    Fixed model view

--- app/views/model_view.py
+++ app/views/model_view.py
@@ -1,206 +1,171 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from app.models import Donor, Donation, Item
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views import View
 import simplejson as json
 
 '''
 DonorView
- - GET: return JSON serialized Donor object
- - POST: Insert into the database and return that object
- - PUT: 
- - DELETE: 
+ - GET: Return JSON serialized Donor object
+ - POST: Insert and return that Donor object
+ - PUT: Update and returnn that Donor object
+ - DELETE: Delete and return HTTP status code
 '''
 class DonorView(View):
     def get(self, request):
         try:
-            id = request.GET['donor_id']
-            donor = Donor.objects.get(id = id)
+            donor = Donor.objects.get(id = request.GET['donor_id'])
             return HttpResponse(json.dumps(donor.serialize()), content_type="application/json", status=200)            
         except:
-            return HttpResponse(json.dumps(None), content_type="application/json", status=404)
+            return HttpResponseBadRequest()
 
     def post(self, request):
         try:
             donor = Donor.objects.create(
-                donor_name = request.GET['donor_name'],
-                email = request.GET['email'],
-                want_receipt = request.GET['want_receipt'],
-                telephone_number = request.GET['telephone_number'],
-                mobile_number = request.GET['mobile_number'],
-                address_line = request.GET['address_line'],
-                city = request.GET['city'],
-                province = request.GET['province'],
-                postal_code = request.GET['postal_code'],
-                customer_ref = request.GET['customer_ref'],
-                verified = request.GET['verified']
+                donor_name = request.POST['donor_name'],
+                email = request.POST['email'],
+                want_receipt = request.POST['want_receipt'],
+                telephone_number = request.POST['telephone_number'],
+                mobile_number = request.POST['mobile_number'],
+                address_line = request.POST['address_line'],
+                city = request.POST['city'],
+                province = request.POST['province'],
+                postal_code = request.POST['postal_code'],
+                customer_ref = request.POST['customer_ref'],
+                verified = request.POST['verified']
             )
+            return HttpResponse(json.dumps(donor.serialize()), content_type="application/json", status=201)
         except:
-            return 
-        return HttpResponse(json.dumps(donor.serialize()), content_type="application/json")
+            return HttpResponseBadRequest() 
 
     def put(self, request):
-        id = request.GET['donor_id']
         try:
-            donor = Donor.objects.get_object_or_404(id = id)
+            donor = Donor.objects.get(id = request.GET['donor_id'])
+            donor.donor_name = request.GET['donor_name'],
+            donor.email = request.GET['email'],
+            donor.want_receipt = request.GET['want_receipt'],
+            donor.telephone_number = request.GET['telephone_number'],
+            donor.mobile_number = request.GET['mobile_number'],
+            donor.address_line = request.GET['address_line'],
+            donor.city = request.GET['city'],
+            donor.province = request.GET['province'],
+            donor.postal_code = request.GET['postal_code'],
+            donor.customer_ref = request.GET['customer_ref'],
+            donor.verified = request.GET['verified']
+            donor.save()
+            return HttpResponse(json.dumps(donor.serialize()), content_type="application/json", status=200)
         except:
-            raise Http404("Donor does not exist")
-        else:
-            donor.update(
-                donor_name = request.GET['donor_name'],
-                email = request.GET['email'],
-                want_receipt = request.GET['want_receipt'],
-                telephone_number = request.GET['telephone_number'],
-                mobile_number = request.GET['mobile_number'],
-                address_line = request.GET['address_line'],
-                city = request.GET['city'],
-                province = request.GET['province'],
-                postal_code = request.GET['postal_code'],
-                customer_ref = request.GET['customer_ref'],
-                verified = request.GET['verified'])
-            return HttpResponse(json.dumps(donor.serialize()), content_type="application/json")
+            return HttpResponseBadRequest()
 
     def delete(self, request):
-        id = request.GET['donor_id']
-        donor = Donor.objects.get_object_or_404(id = id)
-        donor.delete()
-        # should add alter or something that says deletion completed?
-        return HttpResponse(json.dumps(none), content_type="application/json")
+        try:
+            donor = Donor.objects.get(id = request.GET['donor_id'])
+            donor.delete()
+            return HttpResponse(json.dumps(None), content_type="application/json", status=200)
+        except:
+            return HttpResponse(json.dumps(None), content_type="application/json", status=400)
 
 '''
 DonationView
- - GET: return JSON serialized Donation object
- - POST: Insert into the database and return that object
- - PUT: 
- - DELETE: 
+ - GET: Return JSON serialized Donation objects based on donor id
+ - POST: Insert and return that Donation object
+ - PUT: Update and return that Donation object
+ - DELETE: Delete and return HTTP status code
 '''
 class DonationView(View):
-    response_data = []
-
     def get(self, request):
-        donation_list = Donation.objects.select_related().filter(donor_id=request.GET['donor_id'])
-        for donation in donation_list:
-            response_data.append(donation.serialize())
+        donation_list = Donation.objects.filter(donor_id=request.GET['donor_id'])
+        response_data = [donation.serialize() for donation in donation_list]
         return HttpResponse(json.dumps(response_data), content_type='application/json')
 
     def post(self, request):
-        donor_id = request.POST['donor_id']
-        tax_receipt_no = request.POST['tax_receipt_no']
-        donate_date = request.POST['donate_date']
-        verified = request.POST['verified']
-        pick_up = request.POST['pick_up']
         donation = Donation.objects.create(
-            donor_id = donor_id,
-            tax_receipt_no = tax_receipt_no,
-            donate_date = donate_date,
-            verified = verified,
-            pick_up = pick_up)
-        response_data.append(donation.serialize())
-        return HttpResponse(json.dumps(response_data), content_type='application/json')
+            donor_id = request.POST['donor_id'],
+            tax_receipt_no = request.POST['tax_receipt_no'],
+            donate_date = request.POST['donate_date'],
+            verified = request.POST['verified'],
+            pick_up = request.POST['pick_up']
+        )
+        return HttpResponse(json.dumps(donation.serialize()), content_type='application/json')
+
+    def put(self, request):
+        try:
+            donation = Donation.objects.get(tax_receipt_no = request.PUT['tax_receipt_no'])
+            donation.donate_date = request.PUT['donate_date']
+            donation.verified = request.PUT['verified']
+            donation.pick_up = request.PUT['pick_up']
+            donation.save()
+            return HttpResponse(json.dumps(donor.serialize()), content_type='application/json', status=200)
+        except:
+            return HttpResponse(json.dumps(None), content_type='application/json', status=400)
 
     def delete(self, request):
         try:
-            donation = Donation.objects.get(request.DELETE['donor_id'])
-        except (Donation.DoesNotExist):
-            return HttpResponse(json.dumps({"Fail: donation does not exist."}), content_type='application/json')
-        else:
+            donation = Donation.objects.get(tax_receipt_no = request.DELETE['tax_receipt_no'])
             donation.delete()
-            return HttpResponse(json.dumps({"Success"}), content_type='application/json')
-    
-    def put(self, request):
-        try:
-            donor_id = request.PUT['donor_id']
-            tax_receipt_no = request.PUT['tax_receipt_no']
-            donate_date = request.PUT['donate_date']
-            verified = request.PUT['verified']
-            pick_up = request.PUT['pick_up']
-        except KeyError:
-            return HttpResponse(json.dumps("Fail: invalid scheme for update."), content_type='application/json')
-        try:
-            donation = Donation.objects.get(tax_receipt_no = tax_receipt_no)
-        except (Donation.DoesNotExist):
-            return HttpResponse(json.dumps("Fail: donation does not exist."))
-        donation.donor_id = donor_id
-        donation.donate_date = donate_date
-        donation.verified = verified
-        donation.pick_up = pick_up
-        donation.save()
-        return HttpResponseRedirect(reverse())
-
-
-
-    # def delete(self, request):
-    #     return render(...)
+            return HttpResponse(json.dumps(None), content_type="application/json", status=200)
+        except:
+            return HttpResponseBadRequest()
 
 '''
 ItemView
- - GET: return JSON serialized Item object
- - POST: Insert into the database and return that object
- - PUT: 
- - DELETE: 
+ - GET: Return JSON serialized Item objects based on donation id
+ - POST: Insert and return Item object
+ - PUT: Update and return Item object
+ - DELETE: Delete and return HTTP status code
 '''
 class ItemView(View):
-    response_data = []
     def get(self, request):
-        id = request.GET['item_id']
         try:
-            item = Item.objects.get_object_or_404(id = id)
+            item = Item.objects.get(id = request.GET['item_id'])
+            return HttpResponse(json.dumps(item.serialize()), content_type="application/json", status=200)
         except:
-            return HttpResponse(json.dumps(None), content_type="application/json")
-        else:
-            response_data.append(item.serialize())
-            # should add alter or something that says deletion completed?
-            return HttpResponse(json.dumps(response_data), content_type="application/json")
-
-
-    def put(self, request):
-        id = request.GET['item_id']
-        try:
-            itemExist = Item.objects.get_object_or_404(id = id)
-        except:
-            raise Http404("Item does not exist")
-        else:
-            item = Item(
-                QUALITY = 'Medium',
-                tax_receipt_no = request.GET['tax_receipt_no'],
-                description = request.GET['description'],
-                particulars = request.GET['particulars'],
-                manufacturer = request.GET['manufacturer'],
-                model = request.GET['model'],
-                quantity = request.GET['quality'],
-                working = request.GET['working'],
-                condition = request.GET['condition'],
-                quality = request.GET['quality'],
-                batch = request.GET['batch'],
-                value = request.GET['value'],
-                verified = request.GET['verified']
-            )
-            item.save()
-            return HttpResponse(json.dumps(none), content_type="application/json")
+            return HttpResponseBadRequest()
 
     def post(self, request):
-        item = Item(
-            QUALITY = 'Medium',
-            tax_receipt_no = request.GET['tax_receipt_no'],
-            description = request.GET['description'],
-            particulars = request.GET['particulars'],
-            manufacturer = request.GET['manufacturer'],
-            model = request.GET['model'],
-            quantity = request.GET['quality'],
-            working = request.GET['working'],
-            condition = request.GET['condition'],
-            quality = request.GET['quality'],
-            batch = request.GET['batch'],
-            value = request.GET['value'],
-            verified = request.GET['verified']
-        )
-        item.save()
-        return HttpResponse(json.dumps(none), content_type="application/json")
-
+        try:
+            item = Item.objects.create(
+                tax_receipt_no = request.POST['tax_receipt_no'],
+                description = request.POST['description'],
+                particulars = request.POST['particulars'],
+                manufacturer = request.POST['manufacturer'],
+                model = request.POST['model'],
+                quantity = request.POST['quality'],
+                working = request.POST['working'],
+                condition = request.POST['condition'],
+                quality = request.POST['quality'],
+                batch = request.POST['batch'],
+                value = request.POST['value'],
+                verified = request.POST['verified']
+            )
+            return HttpResponse(json.dumps(item.serialize()), content_type="application/json", status=200)
+        except:
+            return HttpResponseBadRequest()
+            
+    def put(self, request
+        try:
+            item = Item.objects.get(id=request.PUT['item_id'])
+            item.tax_receipt_no = request.PUT['tax_receipt_no']
+            item.description = request.PUT['description']
+            item.particulars = request.PUT['particulars']
+            item.manufacturer = request.PUT['manufacturer']
+            item.model = request.PUT['model']
+            item.quantity = request.PUT['quality']
+            item.working = request.PUT['working']
+            item.condition = request.PUT['condition']
+            item.quality = request.PUT['quality']
+            item.batch = request.PUT['batch']
+            item.value = request.PUT['value']
+            item.verified = request.PUT['verified']
+            item.save()
+            return HttpResponse(json.dumps(item.serialize()), content_type="application/json", status=200)
+        except:
+            return HttpResponseBadRequest()
     def delete(self, request):
-        id = request.GET['item_id']
-        item = Item.objects.get(id=id)
-        item.delete()
-        return HttpResponse(json.dumps(none), content_type="application/json")
+        try:
+            item = Item.objects.get(id=request.DELETE['item_id'])
+            item.delete()
+            return HttpResponse(json.dumps(None), content_type="application/json", status=200)
+        except:
+            return HttpResponseBadRequest()
