7bf26eeb3c2a92a35dcfee04b008ef349efa39bd
hurjun1995 <hurjun1995@gmail.com>
2018-03-05 00:19:32 -0800

    branch created for aggregate_value & aggregate_quantity

---
+++ app/views/data_view.py
@@ -0,0 +1,12 @@
+
+
+
+# return aggregate value for given time interval
+@login_required(login_url='/login')
+def aggregate_value(request):
+    # TODO
+
+# return aggregate quantity of given model for given time interval
+@login_required(login_url='/login')
+def aggregate_quality(request):
+    # TODO
