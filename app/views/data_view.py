7de6c4cbac41e4172d16f3d6e7167ca65b0ba408
hurjun1995 <hurjun1995@gmail.com>
2018-03-21 16:36:18 -0700

    fixed logical issue of functions and minor syntax&spelling issues

--- app/views/data_view.py
+++ app/views/data_view.py
@@ -3,10 +3,11 @@ from app.models import Donor, Donation, Item
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views import View
 import simplejson as json
+import time
 
 @login_required(login_url='/login')
 def aggregate_value(request):
-    '''if request.GET['interval'] is false, return value of all items
+    '''If request.GET['interval'] is false, return value of all items
     otherwise, return array of values for given interval
     '''
     try:
@@ -14,55 +15,83 @@ def aggregate_value(request):
         end_date = request.GET['end_date']
 
         items = __getModelsGivenInterval('item', start_date, end_date)
-        
-        item_date_pairs = map(__generatePairs, items)
+
+        item_date_pairs = __generatePairsWithValue(items)
         result = {'result': item_date_pairs}
 
-        return JsonResponse(result.serialize(), status=200)
+        return JsonResponse(result, status=200)
     except BaseException:
         return HttpResponseBadRequest()
 
 
 @login_required(login_url='/login')
-def aggregate_quanity(request):
-    '''return aggregate quantity of given model for given time interval.'''
+def aggregate_quantity(request):
+    '''Return aggregate quantity of given model for given time interval.'''
     try:
         model = request.GET['model']
         start_date = request.GET['start_date']
         end_date = request.GET['end_date']
 
         items = __getModelsGivenInterval(model, start_date, end_date)
-        aggregated_quantity = sum(1 for item in items)
-        result = {'result': aggregate_quanity}
+
+        aggregated_quantity = __generatePairsWithQuantity(items)
+        result = {'result': aggregated_quantity}
         
-        return JsonResponse(result.serialize(), status=200)
+        return JsonResponse(result, status=200)
     except BaseException:
-        return HttpResponseBadRequest
+        return HttpResponseBadRequest()
 
 '''
 Private
 '''
 def __getModelsGivenInterval(model, start_date, end_date):
-    '''returns the given Models in given time interval.'''
-    model_class = {
+    '''Returns the given Models in given time interval.'''
+    cur_model = {
         'donor': Donor,
         'donation': Donation,
         'item': Item
-    }
+    }.get(model, Donor.objects.none())
 
     # TODO: what to do for default case?
-    if start_date != null && end_date != null:
-        return model_Class.get(model, Donor).filter(created_at__range=(start_date, end_date))
+    if start_date != None && end_date != None:
+        return cur_model.filter(created_at__range=(start_date, end_date))
     
-    elif start_date != null && end_date == null:
-        return model_Class.get(model, Donor).objects.filter(created_at__gte=start_date)
+    elif start_date != None && end_date == None:
+        return cur_model.objects.filter(created_at__gte=start_date)
     
-    elif start_date == null && end_date != null:
-        return model_Class.get(model, Donor).objects.filter(created_at__lte=end_date)
+    elif start_date == None && end_date != None:
+        return cur_model.objects.filter(created_at__lte=end_date)
 
-    else # both null
-        return model_Class.get(model, Donor).objects.all()
+    else # both None
+        return cur_model.objects.all()
 
-def __generatePairs(model):
-    '''generate pairs of creation date and value, given element'''
-    return {'date': model.created_at, 'value': model.value}
+def __generatePairsWithValue(objects):
+    '''Generate pairs of creation date and value, given lists of objects'''
+    result = {}
+
+    for item in objects:
+        formatted_date = __formatDate(item.created_at)
+        exists = result.get(formatted_date)
+        if exists != None:  # Already existed
+            result[formatted_date] += item.value
+        else:   # New date added
+            result[formatted_date] = item.value
+
+    return result
+
+def __generatePairsWithQuantity(objects):
+     '''Generate pairs of creation date and quantity, given lists of objects'''
+    result = {}
+
+    for item in objects:
+        formatted_date = __formatDate(item.created_at)
+        exists = result.get(formatted_date)
+        if exists != None:  # Already existed
+            result[formatted_date] += 1
+        else:   # New date added
+            result[formatted_date] = 1
+
+    return result
+
+def __formatDate(date):
+    return time.strftime("%d-%m-%Y", date)
