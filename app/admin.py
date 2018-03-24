998aade62d92bbba7125ee7feb78ff6b4f3869a9
yuubd <yuubd@github.com>
2018-03-24 13:47:11 -0700

    added status actions

--- app/admin.py
+++ app/admin.py
@@ -42,6 +42,43 @@ def make_unverified(modeladmin, request, queryset):
 
 make_unverified.short_description = "Mark as unverified"
 
+
+def make_pledge(modeladmin, request, queryset): 
+    queryset.update(status='pledge')
+
+make_pledge.short_description = "Mark as pledge"
+
+
+def make_received(modeladmin, request, queryset): 
+    queryset.update(status='received')
+
+make_received.short_description = "Mark as received"
+
+
+def make_tested(modeladmin, request, queryset): 
+    queryset.update(status='tested')
+
+make_tested.short_description = "Mark as tested"
+
+
+def make_refurbished(modeladmin, request, queryset): 
+    queryset.update(status='refurbished')
+
+make_refurbished.short_description = "Mark as reburbished"
+
+
+def make_sold(modeladmin, request, queryset): 
+    queryset.update(status='sold')
+
+make_sold.short_description = "Mark as sold"
+
+
+def make_recycled(modeladmin, request, queryset): 
+    queryset.update(status='recycled')
+
+make_recycled.short_description = "Mark as recycled"
+
+
 # Action for generating pdf
 
 
@@ -115,9 +152,10 @@ class ItemAdmin(admin.ModelAdmin):
                     'manufacturer',
                     'model',
                     'quantity',
-                    'batch',
+                    'status',
                     'verified',
-                    'get_donor_name')     # david/status one more column, status 
+                    'batch'
+                    )     
     list_filter = ['working', 'verified', 'quality']
     search_fields = ['manufacturer', 'model', 'working', 'batch',
                      'tax_receipt_no__tax_receipt_no', 'tax_receipt_no__donor_id__donor_name']
@@ -125,8 +163,17 @@ class ItemAdmin(admin.ModelAdmin):
     def get_item(self, obj):
         return obj.id
     get_item.short_description = 'Item Id'
-
-    actions = [make_verified, make_unverified]    # david/status add status actions for each
+    
+    actions = [
+        make_verified, 
+        make_unverified, 
+        make_pledge, 
+        make_received, 
+        make_tested, 
+        make_refurbished, 
+        make_sold, 
+        make_recycled
+        ]
 
     def get_donor_name(self, obj):
         return obj.tax_receipt_no.donor_id.donor_name
@@ -140,3 +187,4 @@ admin.site.register(Donor, DonorAdmin)
 admin.site.register(Donation, DonationAdmin)
 
 admin.site.register(Item, ItemAdmin)
+
