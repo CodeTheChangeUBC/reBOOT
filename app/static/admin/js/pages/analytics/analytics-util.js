a575fecdec8c09a98f84cce8978b393b6d36c4d7
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-03-24 18:54:52 -0700

    Move the data retrieval process into util

--- app/static/admin/js/pages/analytics/analytics-util.js
+++ app/static/admin/js/pages/analytics/analytics-util.js
@@ -1,6 +1,17 @@
 "use strict";
 
 define(function() {
+  var totalQuantityObj = {
+    // donor: [],
+    // donation: [],
+    item: []
+  };
+  var totalValueObj = {
+    // donor: [],
+    // donation: [],
+    item: []
+  };
+
   var cookie = (function(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== "") {
@@ -52,8 +63,62 @@ define(function() {
     });
   };
 
+  var totalQuantity = function(model, startDate, endDate, force) {
+    if (force || totalQuantityObj[model].length === 0) {
+      return aggregateQuantity(model, startDate, endDate).then(function(data) {
+        totalQuantityObj[model] = data.result;
+        return totalQuantityObj[model];
+      });
+    }
+    return totalQuantityObj[model];
+  };
+
+  var totalQuantityAll = function(startDate, endDate, force) {
+    var promises = [];
+    $.each(totalQuantityObj, function(key) {
+      promises.push(
+        new Promise(function(resolve) {
+          totalQuantity(key, startDate, endDate, force).then(function(data) {
+            resolve(data);
+          });
+        })
+      );
+    });
+    return Promise.all(promises).then(function() {
+      return totalQuantityObj;
+    });
+  };
+
+  var totalValue = function(model, startDate, endDate, force) {
+    if (force || totalValueObj[model].length === 0) {
+      return aggregateValue(model, startDate, endDate).then(function(data) {
+        totalValueObj[model] = data.result;
+        return totalValueObj[model];
+      });
+    }
+    return totalValueObj[model];
+  };
+
+  var totalValueAll = function(startDate, endDate, force) {
+    var promises = [];
+    $.each(totalValueObj, function(key) {
+      promises.push(
+        new Promise(function(resolve) {
+          totalValue(key, startDate, endDate, force).then(function(data) {
+            resolve(data);
+          });
+        })
+      );
+    });
+    return Promise.all(promises).then(function() {
+      return totalValueObj;
+    });
+  };
+
   return {
-    aggregateQuantity: aggregateQuantity,
-    aggregateValue: aggregateValue
+    totalValue: totalValue,
+    totalValueAll: totalValueAll,
+    totalQuantity: totalQuantity,
+    totalQuantityAll: totalQuantityAll
   };
 });
