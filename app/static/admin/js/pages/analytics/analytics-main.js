1dc6d2fe8f64cb83b6f41aa266cee72ff5adb644
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-03-28 20:20:45 -0700

    wip

--- app/static/admin/js/pages/analytics/analytics-main.js
+++ app/static/admin/js/pages/analytics/analytics-main.js
@@ -2,6 +2,13 @@
 
 define(function(require) {
   var quickSummary = require("./quick-summary");
+  quickSummary.getRangedData().then(function(data) {
+    console.log(data);
+  });
+
+  quickSummary.getTotalData().then(function(data) {
+    console.log(data);
+  });
 
   //   var itemPercentagePieChart = require("./item-percentage");
   //   var itemLocationBarChart = require("./item-location");
