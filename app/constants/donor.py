5ef0db7817aeb741820d027a8e210c6061a974fe
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-05-05 13:44:19 -0700

    Fixed the HttpBadRequest return statement + refactored model constants

---
+++ app/constants/donor.py
@@ -0,0 +1,15 @@
+PROVINCE = {
+    ('AB', 'Alberta'),
+    ('BC', 'British Columbia'),
+    ('SK', 'Saskatchewan'),
+    ('MB', 'Manitoba'),
+    ('ON', 'Ontario'),
+    ('QC', 'Quebec'),
+    ('PE', 'Prince Edward Island'),
+    ('NS', 'Nova Scotia'),
+    ('NL', 'Newfoundland and Labrador'),
+    ('NB', 'New Brunswick'),
+    ('NT', 'Northwest Territories'),
+    ('NU', 'Nunavut'),
+    ('YT', 'Yukon')
+}
