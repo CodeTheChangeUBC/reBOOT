18887b45cff3a585081ec57ad866d555bbe7b15d
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-05-26 16:18:55 -0700

    Vendor cleanup and calender option fix. Closes #171. Closes #140
     # Please enter the commit message for your changes. Lines starting

--- reboot/settings.py
+++ reboot/settings.py
@@ -140,14 +140,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
