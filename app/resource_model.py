029ce98190c15ebd3907c2693a718b111cde83b2
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-06-26 22:45:24 -0700

    Refactored serialization code into resource model

--- app/resource_model.py
+++ app/resource_model.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.db.models.query import QuerySet
 from django.utils import timezone
-from datetime import datetime
+from datetime import datetime, date
+import simplejson as json
+import re
 
 
 class ResourceManager(models.Manager):
@@ -35,6 +37,12 @@ class ResourceModel(models.Model):
             self.documented_at = datetime.utcnow().strftime("%Y-%m-%d")
         super(ResourceModel, self).save(*args, **kwargs)
 
+    def underscore_serialize(self):
+        return _underscore_serialize(self)
+
+    def camel_serialize(self):
+        return _camel_serialize(self)
+
     def delete(self):
         self.deleted_at = datetime.utcnow()
         self.save()
@@ -55,3 +63,47 @@ class ResourceQuerySet(QuerySet):
 
     def dead(self):
         return self.exclude(deleted_at=None)
+
+
+'''
+Private Method
+'''
+
+
+def _underscore_serialize(self):
+    serialized_dict = self.__dict__
+    if '_state' in serialized_dict:
+        serialized_dict.pop('_state')
+    json_str = json.dumps(serialized_dict, default=_json_serial)
+    return json.loads(json_str)
+
+def _camel_serialize(self):
+    serialized_dict = self.__dict__
+    if '_state' in serialized_dict:
+        serialized_dict.pop('_state')
+    cameled_dict = _convert_json(serialized_dict, _underscore_to_camel)
+    json_str = json.dumps(cameled_dict, default=_json_serial)
+    return json.loads(json_str)
+
+def _convert_json(d, convert):
+    new_d = {}
+    for k, v in d.iteritems():
+        v = v if not isinstance(v, dict) else convert_json(v, convert)
+        new_d[convert(k)] = v
+    return new_d
+
+def _underscore_to_camel(name):
+    under_pat = re.compile(r'_([a-z])')
+    return under_pat.sub(lambda x: x.group(1).upper(), name)
+
+
+def _json_serial(obj):
+    """JSON serializer for objects not serializable by default json code
+    """
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    if isinstance(obj, Donor):
+        return obj.id
+    if isinstance(obj, Donation):
+        return obj.tax_receipt_no
+    raise TypeError("Type %s not serializable" % type(obj))
