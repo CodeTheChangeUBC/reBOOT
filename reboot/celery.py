b0dd1d4dda93a5be65c28ec2769ce57f95b05bd3
Tom Lee <win981026@gmail.com>
2019-06-07 02:28:35 -0700

    fix: celery-amqp connection refused
    Closes #207

--- reboot/celery.py
+++ reboot/celery.py
@@ -5,14 +5,13 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reboot.settings')
 
-app = Celery('reboot', backend=settings.CELERY_BACKEND_TYPE)
-
-app.conf.update(
+app = Celery(
+    'reboot',
     broker=settings.CELERY_BROKER_URL,
     backend=settings.CELERY_RESULT_BACKEND,
     task_serializer=settings.CELERY_TASK_SERIALIZER,
     accept_content=settings.CELERY_ACCEPT_CONTENT,
-    result_serializer=settings.CELERY_RESULT_SERIALIZER,
+    result_serializer=settings.CELERY_RESULT_SERIALIZER
 )
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
