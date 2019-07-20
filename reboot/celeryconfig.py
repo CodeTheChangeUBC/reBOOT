f2b59933035d75ecec0235af9685d8206aa2fe3f
Tom Lee <win981026@gmail.com>
2019-07-19 20:22:48 -0700

    devop: Fix celeryconfig for dev

--- reboot/celeryconfig.py
+++ reboot/celeryconfig.py
@@ -6,7 +6,6 @@ broker_url = config('CLOUDAMQP_URL', default='amqp://guest@localhost//')
 broker_connection_timeout = 30
 broker_heartbeat = None
 broker_pool_limit = 1
-broker_use_ssl = True
 event_queue_expires = 60
 worker_prefetch_multiplier = 1
 worker_concurrency = 10
@@ -19,7 +18,7 @@ result_serializer = 'pickle'
 if config('CLOUDAMQP_URL', default=False):
     broker_use_ssl = True
 else:
-    BROKER_USE_SSL = {
+    broker_use_ssl = {
         'keyfile': 'dev/ssl/cert/client_key.pem',
         'certfile': 'dev/ssl/cert/client_certificate.pem',
         'ca_certs': 'dev/ssl/cert/ca_certificate.pem',
