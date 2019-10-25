0cf49240c71d177e6a0e8c48e8e9bb33397ac979
Tom Lee <win981026@gmail.com>
2019-10-24 21:50:48 -0700

    feat: Add perms for 3rd party and web importer

--- app/models/all.py
+++ app/models/all.py
@@ -8,4 +8,6 @@ class All(models.Model):
                          # operations will be performed for this model.
         default_permissions = ()
         permissions = (('can_import_historical', 'Can import historical data'),
+                       ('can_import_third_party', 'Can import third party data'),
+                       ('can_import_website', 'Can import website data'),
                        ('can_export_data', 'Can export data'),)
