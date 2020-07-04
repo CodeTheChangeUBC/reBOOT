929ee9517b814b9661d687ebfb51d33583f8d54e
Tom Lee <win981026@gmail.com>
2020-07-04 22:18:29 +0900

    Modified webform importer to include notes

--- app/worker/tasks/importers/webform_data_importer.py
+++ app/worker/tasks/importers/webform_data_importer.py
@@ -12,12 +12,13 @@ class WebformDataImporter(BaseCsvImporter):
     :param str csvfile: csvfile path
     """
 
-    bulk_model = Donation
+    # bulk_model = Donation
 
     def parse_row(self, row):
         donor = self._goc_donor(self._parse_donor(row))
-        self.model_bulk.append(
-            self._new_donation(self._parse_donation(row), donor))
+        self._create_donation(self._parse_donation(row), donor)
+        # self.model_bulk.append(
+        #     self._new_donation(self._parse_donation(row), donor))
 
     def _parse_donor(self, row):
         """Takes a csv row and parses relevant Donor data
@@ -26,10 +27,8 @@ class WebformDataImporter(BaseCsvImporter):
         :return: Donor data dict
         :rtype: dict
         """
-        want_receipt = {
-            "yes": "EMAIL",
-            "no": "REFUSED",
-        }.get(row["Tax Receipt Required"].lower(), "EMAIL")
+        want_receipt = row["Tax Receipt Required"].lower()
+        want_receipt = "REFUSED" if want_receipt == "no" else "EMAIL"
         donor_name = row["Business Name (if applicable)"]
         contact_name = " ".join(filter(ops.truth, [
             row["Contact Name (First)"],
@@ -43,40 +42,69 @@ class WebformDataImporter(BaseCsvImporter):
             "contact_name": contact_name,
             "email": row["Email Address"],
             "want_receipt": want_receipt,
-            "telephone_number": row["Phone Number"],
-            "mobile_number": "",
+            "telephone_number": "",
+            "mobile_number": row["Phone Number"],
             "address_line_one": row["Address (Street Address)"],
             "address_line_two": row["Address (Address Line 2)"],
             "city": row["Address (City)"],
             "province": row["Address (State / Province)"],
-            "postal_code": row["Address (ZIP / Postal Code)"],
+            "postal_code": row["Address (ZIP / Postal Code)"].upper(),
             # "customer_ref": "",
             "documented_at": self._parse_date(row["Entry Date"]),
         }
 
     def _parse_donation(self, row):
+        note = row["Notes: (parking, buzzer, dock availability, stairs, etc.)"]
+        links = row["Images or Inventory List"]
+        certificate = row["Do you require a certificate of data erasure?"]
+        certificate = "Yes" if certificate.lower() == "yes" else "No"
+        received_by = row["1"]
+        items = "\n".join(self._parse_items(row))
+
         return {
             "pledge_date": parse(row["Entry Date"]).date(),
-            # "pick_up": row[""],  # ?
+            "pick_up": row["Address (ZIP / Postal Code)"].upper(),
             "source": "WEBSITE_IMPORT",  # fixed
             "documented_at": self._parse_date(row["Entry Date"]),
-            # "notes": "",
+            "notes": (
+                f"Received By: {received_by}\n",
+                f"Links: {links}\n",
+                f"Require Certificate of Data Erasure?: {certificate}\n",
+                f"Items: {items}\n",
+                f"Additional Notes: {note}",
+            ),
         }
-        pass
+
+    def _parse_items(self, row):
+        items = [colval.strip() for (colkey, colval) in row.items()
+                 if "Donation Items" in colkey]
+        return filter(ops.truth, items)
 
     def _goc_donor(self, data):
         """get_or_create a Donor
-: param dict data: A Donor data dict: return: Donor object got_or_created from the data        : rtype: app.models.Donor:         """
-        donor, unique = Donor.objects.get_or_create(**data) return donor
-
-    def _new_donation(self, data, donor):
-        """Initialize a new Donation object
-: param dict data: A Donation data dict: param app.models.Donor donor: A Donor object: return: Donation object from data to be bulk created
-        : rtype app.models.Donation: Donation object
+        :param dict data: A Donor data dict
+        :return: Donor object got_or_created from the data
+        :rtype: app.models.Donor
         """
         try:
-            d = Donation(donor=donor, **data)
-            d.clean_fields()
+            d = Donor.objects.get(
+                contact_name=data.get("contact_name"),
+                email=data.get("email"),
+                mobile_number=data.get("mobile_number"))
+        except Exception:
+            d = Donor.objects.create(**data)
+        return d
+
+    def _create_donation(self, data, donor):
+        """Create a new Donation object
+
+        :param dict data: A Donation data dict
+        :param app.models.Donor donor: A Donor object
+        :return: Donation object from data
+        :rtype: app.models.Donation: Donation object
+        """
+        try:
+            d = Donation.objects.create(donor=donor, **data)
         except Exception as e:
             self.logger.error(f"Donation data: {d.underscore_serialize()}")
             raise e
