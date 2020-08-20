cfde3f8887138eeeb3789e97e334e10f7cca4b1c
Tom Lee <win981026@gmail.com>
2020-08-20 17:03:08 +0900

    Abstracted common words from string constants
    - Added empty donations (with no item) error message

--- app/constants/str.py
+++ app/constants/str.py
@@ -1,6 +1,9 @@
 UNCHANGEABLE_ERROR = 'This instance may not be modified further since the related tax receipt was generated.'
 PERMISSION_DENIED = 'Permission denied. Please contact admin for access.'
-UNVERIFIED_DONATION = 'Unverified donations are not valid for tax receipt generation. Please review and try again.'
-UNEVALUATED_DONATION = 'Donations with not evaluated items are not valid for tax receipt generation. Please review and try again.'
-RECEIPTED_DONATION = 'Donations with tax receipts already generated are not valid for tax receipt generation. Please review and try again.'
+
+BASE_DONATION = 'are not valid for tax receipt generation. Please review and try again.'
+UNVERIFIED_DONATION = f'Unverified donations {BASE_DONATION}'
+UNEVALUATED_DONATION = f'Donations with not evaluated items {BASE_DONATION}'
+RECEIPTED_DONATION = f'Previously receipted donations {BASE_DONATION}'
+EMPTY_DONATION = f'Donations with no items {BASE_DONATION}'
 DONATION_EVENT_ORDER_ERROR = 'Donation events must occur in this order: pledged, received, tested, valuated, and receipted. Please set previous event dates.'
