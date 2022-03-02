71de41cf4378bd2ee049e769499a38dbcdcff612
GwangChul Kim <gc.kim93@gmail.com>
2022-03-01 19:17:21 -0800

    feat: Added autocomplete feature (#327)
    * Added autocomplete feature
    
    * minor fix
    
    * addressed feedback
    
    * fixed Line too long
    
    * Addressed feedback
    
    * indent fix

--- app/urls.py
+++ app/urls.py
@@ -38,6 +38,8 @@ urlpatterns = [
 # API urlpatterns
 urlpatterns += [
     url(r'^api/autocomplete_name$', api_views.autocomplete_name),
+    url(r'^api/donor_names$', api_views.donor_names),
+    url(r'^api/device_names$', api_views.device_names),
     url(r'^api/related_donations$', api_views.related_donations),
     url(r'^api/related_items$', api_views.related_items),
     url(r'^api/quantity$', data_view.aggregate_quantity, name='aggregate_quantity'),
--- app/views/api_views.py
+++ app/views/api_views.py
@@ -1,4 +1,4 @@
-from app.models import Donor, Donation, Item
+from app.models import Donor, Donation, Item, ItemDevice
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_GET
@@ -11,13 +11,41 @@ def autocomplete_name(request):
     try:
         search_key = request.GET['key']
         search_result = Donor.objects.filter(
-            donor_name__icontains=search_key
-        ).order_by('id')
+            donor_name__icontains=search_key).order_by('id')
         search_result = [donor.camel_serialize() for donor in search_result]
         return HttpResponse(json.dumps(search_result),
                             content_type="application/json")
     except:
         return HttpResponse('', content_type="application/json")
+
+
+@login_required(login_url='/login')
+@require_GET
+def donor_names(request):
+    try:
+        donor_names = list(Donor.objects.values_list(
+            "donor_name", flat=True).order_by('donor_name'))
+        return JsonResponse({'donorNames': donor_names},
+                            content_type="application/json")
+    except Exception as e:
+        return JsonResponse(e, safe=False)
+
+
+@login_required(login_url='/login')
+@require_GET
+def device_names(request):
+    try:
+        device_objs = []
+        for device in ItemDevice.objects.all():
+            if device.dtype is not None:
+                deviceInfo = f"{device.dtype.device_type} \
+                    ({device.make}-{device.model})"
+                device_objs.append(deviceInfo)
+
+        return JsonResponse({'deviceNames': device_objs},
+                            content_type="application/json")
+    except Exception as e:
+        return JsonResponse(e, safe=False)
 
 
 @login_required(login_url='/login')
