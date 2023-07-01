37a947c7084018110fd0635e76cfa3699244ee5e
Aaron Lane <2400330-aaron-lane@users.noreply.gitlab.com>
2023-06-30 20:19:49 -0400

    Convert donor test to Django test framework

--- app/models/test_donor.py
+++ app/models/test_donor.py
@@ -1,64 +1,69 @@
+from django.test import TestCase
 
-from app import models
-from app.models import Donation
 from app.models import Donor
-from django.db import IntegrityError
-
-import pytest
-
-pytestmark = pytest.mark.django_db
+from django.db.utils import DataError
 
 
-@pytest.mark.django_db
-class TestUsers:
-    pytestmark = pytest.mark.django_db
-
-
-def f():
-    raise SystemExit(1)
-
-
-def test_mytest():
-    with pytest.raises(SystemExit):
-        f()
-
-
-def test_donor_with_complete_fields():
-    test_donor = Donor(donor_name='Tesla', email='NickT@gmail.com', want_receipt=True,
-                       telephone_number='604-123-5678', mobile_number='604-123-5678',
-                       address_line='Mars street', city='Tokyo',
-                       province='Japan', postal_code='123123', verified=True)
-    test_donor.save()
-
-    response = Donor.objects.get(donor_name='Tesla')
-    assert response.donor_name == 'Tesla'
-    assert response.email == 'NickT@gmail.com'
-    assert response.want_receipt == True
-    assert response.telephone_number == '604-123-5678'
-    assert response.mobile_number == '604-123-5678'
-    assert response.address_line == 'Mars street'
-    assert response.city == 'Tokyo'
-    assert response.province == 'Japan'
-    assert response.postal_code == '123123'
-    assert response.verified == True
-
-
-def test_donor_bad_phonenumber():
-    # DOES NOT THROW ANYTHING.
-    test_donor = Donor(donor_name='Zavala',
-                       telephone_number='111122223333123', want_receipt=False)
-    test_donor.save()
-
-
-def test_donor_null_for_want_receipt():
-    with pytest.raises(IntegrityError):
-        test_donor = Donor(donor_name='Tohru', telephone_number='123-333-3333')
+class DonorTestCase(TestCase):
+    def test_complete_arguments(self):
+        test_donor = Donor(
+            donor_name="Tesla",
+            email="NickT@gmail.com",
+            want_receipt="Mail",
+            telephone_number="604-123-5678",
+            mobile_number="604-123-5678",
+            address_line_one="Mars street",
+            address_line_two="Unit 2",
+            city="Tokyo",
+            province="Japan",
+            postal_code="123123",
+        )
         test_donor.save()
 
+        response = Donor.objects.get(donor_name="Tesla")
+        self.assertEqual(response.donor_name, "Tesla", "donor_name")
+        self.assertEqual(response.email, "NickT@gmail.com", "email")
+        self.assertEqual(response.want_receipt, "Mail", "want_receipt")
+        self.assertEqual(response.telephone_number, "604-123-5678", "telephone_number")
+        self.assertEqual(response.mobile_number, "604-123-5678", "mobile_number")
+        self.assertEqual(response.address_line_one, "Mars street", "address_line_one")
+        self.assertEqual(response.address_line_two, "Unit 2", "address_line_two")
+        self.assertEqual(response.city, "Tokyo", "city")
+        self.assertEqual(response.province, "Japan", "province")
+        self.assertEqual(response.postal_code, "123123", "postal_code")
+        self.assertEqual(response.verified(), True, "verified()")
 
-def test_invalid_postal_code():
-    test_donor = Donor(donor_name='Kobayashi',
-                       postal_code='12345678123afasdf', want_receipt=False)
-    test_donor.save()
-    response = Donor.objects.get(donor_name='Kobayashi')
-    assert len(response.postal_code) == 7
+    def test_invalid_telephone_number(self):
+        raised = False
+
+        try:
+            # DOES NOT THROW ANYTHING.
+            test_donor = Donor(
+                donor_name="Zavala",
+                telephone_number="111122223333123",
+                want_receipt=False,
+            )
+            test_donor.save()
+        except:
+            raised = True
+
+        self.assertFalse(
+            raised,
+            "Saving a donor with an invalid telephone number should not raise an exception",
+        )
+
+    def test_default_want_receipt(self):
+        test_donor = Donor(donor_name="Tohru", telephone_number="123-333-3333")
+        test_donor.save()
+
+        response = Donor.objects.get(donor_name="Tohru")
+        self.assertEqual(response.want_receipt, "EMAIL")
+
+    def test_invalid_postal_code(self):
+        with self.assertRaisesRegex(DataError, "value too long"):
+            test_donor = Donor(
+                donor_name="Kobayashi",
+                postal_code="12345678123afasdf",
+                want_receipt=False,
+            )
+            test_donor.save()
