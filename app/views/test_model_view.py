2960e9007953a2f74807eb0380f108d4e099e4ab
Aaron Lane <aaron.lane@hashicorp.com>
2023-07-29 19:08:28 +0000

    Organize test code

--- app/views/test_model_view.py
+++ app/views/test_model_view.py
@@ -7,31 +7,32 @@ from django.utils.http import urlencode
 class DonorViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
+        self.url_path = "/api/donor"
+
         user_password = "testing"
         self.user = User.objects.create_superuser(
             "tester", email="tester@example.com", password=user_password
         )
         self.client.login(username=self.user.username, password=user_password)
 
-        self.test_donor_name = "Test Donor"
-        Donor.objects.create(donor_name=self.test_donor_name)
+        self.test_donor = Donor.objects.create(donor_name="Test Donor")
 
     def test_get(self):
-        donor = Donor.objects.get(donor_name=self.test_donor_name)
-        response = self.client.get("/api/donor", {"id": donor.id})
+        response = self.client.get(self.url_path, {"id": self.test_donor.id})
         response_json = response.json()
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_json["donorName"], donor.donor_name)
+        self.assertEqual(response_json["donorName"], self.test_donor.donor_name)
 
     def test_post(self):
-        donor_name = "Other Test Donor"
-        email = "other-test-donor@example.com"
+        want_donor_name = "Other Test Donor"
+        want_email = "other-test-donor@example.com"
+
         response = self.client.post(
-            "/api/donor",
+            self.url_path,
             {
-                "donorName": donor_name,
-                "email": email,
+                "donorName": want_donor_name,
+                "email": want_email,
                 "wantReceipt": "true",
                 "telephoneNumber": "+1 (234) 576-8901",
                 "mobileNumber": "+2 (345) 678-9012",
@@ -43,18 +44,18 @@ class DonorViewTestCase(TestCase):
                 "customerRef": "Unknown",
             },
         )
-        donor = Donor.objects.get(donor_name=donor_name)
+        donor = Donor.objects.get(donor_name=want_donor_name)
 
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(donor.email, email)
+        self.assertEqual(donor.email, want_email)
 
     def test_put(self):
-        donor = Donor.objects.get(donor_name=self.test_donor_name)
-        updated_donor_name = "Best Donor"
+        want_donor_name = "Best Donor"
+
         data = urlencode(
             {
-                "id": donor.id,
-                "donorName": updated_donor_name,
+                "id": self.test_donor.id,
+                "donorName": want_donor_name,
                 "email": "test@example.com",
                 "wantReceipt": "true",
                 "telephoneNumber": "+1 (234) 576-8901",
@@ -68,20 +69,19 @@ class DonorViewTestCase(TestCase):
             }
         )
         response = self.client.put(
-            "/api/donor",
+            self.url_path,
             data,
             content_type="application/x-www-form-urlencoded",
         )
-        donor.refresh_from_db()
+        self.test_donor.refresh_from_db()
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(donor.donor_name, updated_donor_name)
+        self.assertEqual(self.test_donor.donor_name, want_donor_name)
 
     def test_delete(self):
-        donor = Donor.objects.get(donor_name=self.test_donor_name)
-        data = urlencode({"id": donor.id})
-        response = self.client.delete("/api/donor", data)
-        donors = Donor.objects.filter(donor_name=donor.donor_name)
+        data = urlencode({"id": self.test_donor.id})
+        response = self.client.delete(self.url_path, data)
+        donors = Donor.objects.filter(donor_name=self.test_donor.donor_name)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(donors), 0)
