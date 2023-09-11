4c369b2181ab25d1540c8660751639359a534f2a
likeablecouches <76267484+likeablecouches@users.noreply.github.com>
2023-09-11 16:34:37 +0000

    Fix errors with specsheet pull request

--- specsheet/models.py
+++ specsheet/models.py
@@ -45,18 +45,18 @@ class RefurbishedItem(models.Model):
     windows_os_no = models.IntegerField("Windows version (ex. '10' for Windows 10)", null=True, default=None)
 
     storage_size = models.IntegerField("Storage size (GB)", null=True, default=None)
-    storage_type = models.CharField("Storage type", choices=storage_type_choices, null=True, default=None)
+    storage_type = models.CharField("Storage type", max_length=200, choices=storage_type_choices, null=True, default=None)
 
     screen_size = models.IntegerField("Screen size (inches)", null=True, default=None)
-    screen_port = models.CharField("Screen port", choices=screen_port_choices, null=True, default=None, blank=True)
+    screen_port = models.CharField("Screen port", max_length=200, choices=screen_port_choices, null=True, default=None, blank=True)
 
-    audio_features = models.CharField("Audio features", choices=audio_feature_choices, null=True, default=None)
+    audio_features = models.CharField("Audio features", max_length=200, choices=audio_feature_choices, null=True, default=None)
 
     battery_ok_or_charged = models.BooleanField("OK/Charged?", default=None)
     ram_size = models.IntegerField("Ram size (GB)", null=True, default=None)
-    LAN_type = models.CharField("LAN type", choices=LAN_type_choices, null=True, default=None)
+    LAN_type = models.CharField("LAN type", max_length=200, choices=LAN_type_choices, null=True, default=None)
 
-    installed_apps = models.CharField("Pre-installed apps", choices=installed_apps_choices, null=True, default=None, blank=True)
+    installed_apps = models.CharField("Pre-installed apps", max_length=200, choices=installed_apps_choices, null=True, default=None, blank=True)
     misc_info = models.TextField("Miscellaneous info", null=True, default=None, blank=True)
     
     def __str__(self):
