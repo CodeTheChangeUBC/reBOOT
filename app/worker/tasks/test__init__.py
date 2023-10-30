e3e75ee1516fb0f68caa4f57d62bb7040bdb72e9
Aaron Lane <aaron.lane@hashicorp.com>
2023-10-30 00:57:41 +0000

    Add receiptor task multiple files test

--- app/worker/tasks/test__init__.py
+++ app/worker/tasks/test__init__.py
@@ -10,12 +10,12 @@ from app.worker import tasks
 
 class InitTestCase(TestCase):
     def setUp(self) -> None:
+        self.donor = Donor.objects.create()
 
         return super().setUp()
 
-    def test_receiptor(self) -> None:
-        donor = Donor.objects.create()
-        Donation.objects.create(donor=donor, donate_date="2023-09-30")
+    def test_receiptor_single_file(self) -> None:
+        Donation.objects.create(donor=self.donor, donate_date="2023-09-30")
         queryset = Donation.objects.all()
         queryset_json = serializers.serialize(format="json", queryset=queryset)
         total_count = Donation.objects.count()
@@ -26,3 +26,17 @@ class InitTestCase(TestCase):
         self.assertEqual(first=response.status_code, second=200)
         self.assertEqual(
             first=response['Content-Type'], second="application/pdf")
+
+    def test_receiptor_multiple_files(self) -> None:
+        Donation.objects.create(donor=self.donor, donate_date="2023-09-30")
+        Donation.objects.create(donor=self.donor, donate_date="2023-10-29")
+        queryset = Donation.objects.all()
+        queryset_json = serializers.serialize(format="json", queryset=queryset)
+        total_count = Donation.objects.count()
+
+        response = tasks.receiptor(
+            queryset=queryset_json, total_count=total_count)
+
+        self.assertEqual(first=response.status_code, second=200)
+        self.assertEqual(
+            first=response['Content-Type'], second="application/zip")
