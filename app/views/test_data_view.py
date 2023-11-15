287b9df3fbbd67ff9e524acda4b95f7266a1069a
Aaron Lane <aaron.lane@hashicorp.com>
2023-11-14 22:00:59 -0500

    Add test_aggregate_location

--- app/views/test_data_view.py
+++ app/views/test_data_view.py
@@ -11,24 +11,33 @@ from app.models.item_device import ItemDevice
 
 class DataViewTestCase(TestCase):
     def setUp(self) -> None:
-        self.client = Client()
-
-        user_password = "testing"
-        self.user = User.objects.create_superuser(
-            "tester", email="tester@example.com", password=user_password)
-        self.client.login(username=self.user.username, password=user_password)
-
-        return super().setUp()
-
-    def test_aggregate_value(self) -> None:
         donor = Donor.objects.create()
         donation = Donation.objects.create(donor=donor)
         item_device = ItemDevice.objects.create()
         Item.objects.create(donation=donation, device=item_device,
                             quantity=2, working=True, value=123)
 
+        user_password = "testing"
+        self.user = User.objects.create_superuser(
+            "tester", email="tester@example.com", password=user_password)
+
+        self.client = Client()
+        self.client.login(username=self.user.username, password=user_password)
+
+        return super().setUp()
+
+    def test_aggregate_value(self) -> None:
         response = self.client.get(path="/api/value")
         response_json = response.json()
+        total_value = response_json["result"][0]["total_value"]
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_json["result"][0]["total_value"], 246.0)
+        self.assertEqual(first=response.status_code, second=200)
+        self.assertEqual(first=total_value, second=246.0)
+
+    def test_aggregate_location(self) -> None:
+        response = self.client.get(path="/api/location")
+        response_json = response.json()
+        count = response_json["result"][0]["count"]
+
+        self.assertEqual(first=response.status_code, second=200)
+        self.assertEqual(first=count, second=1)
